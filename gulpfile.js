fc9c8a8d7706f1d991b985d88398e3831f834b93
Ilya Vento <ilya.popov.sbmt@gmail.com>
2018-10-14 23:16:00 +0300

    Some optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,41 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
 
-gulp.task('scss', () =>
+gulp.task("scss", () =>
   gulp
-    .src('dev/scss/**/*.scss')
+    .src("dev/scss/**/*.scss")
     .pipe(sass())
     .pipe(
-      autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
-        cascade: true,
+      autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
+        cascade: false
       })
     )
     .pipe(cssnano())
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest("./public/css"))
 );
 
-gulp.task('js', () =>
+gulp.task("js", () =>
   gulp
-    .src('./dev/js/**/*.js')
-    .pipe(concat('index.js'))
-    .pipe(uglify('index.js'))
-    .pipe(gulp.dest('./public/js'))
+    .src("./dev/js/index.js")
+    //.pipe(concat('index.js'))
+    .pipe(uglify("index.js"))
+    .pipe(gulp.dest("./public/js"))
 );
 
-gulp.task('default', ['scss', 'js'], () => {
-  gulp.watch('dev/scss/**/*.scss', ['scss']);
-  gulp.watch('dev/js/**/*.js', ['js']);
+gulp.task("unitegallery", () =>
+  gulp
+    .src(["./dev/js/ug-theme-tiles.js", "./dev/js/unitegallery.js"])
+    .pipe(concat("unitegallery.js"))
+    .pipe(uglify("unitegallery.js"))
+    .pipe(gulp.dest("./public/js"))
+);
+
+gulp.task("default", ["scss", "js", "unitegallery"], () => {
+  gulp.watch("dev/scss/**/*.scss", ["scss"]);
+  gulp.watch("dev/js/index.js", ["js"]);
+  gulp.watch(["./dev/js/ug-theme-tiles.js", "./dev/js/unitegallery.js"], ["unitegallery"]);
 });
